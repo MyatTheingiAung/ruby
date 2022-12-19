9e4337766483f6e61046edd113f693443946ddfa
MyatTheingiAung <scm.myattheingiaung@gmail.com>
2022-12-19 17:01:19 +0630

    review fix tutorial 6

--- tutorial_six.rb
+++ tutorial_six.rb
@@ -11,17 +11,6 @@ if choose == "save"
   File.open("tutorial_file/#{title}.txt", "w") do |file|     
     file.write(note)   
   end
+  puts "The file title is #{title}"
+  puts "The file note is #{note}"
 end
-
-begin
-  print "Enter open file name : "
-  read_title = gets.chomp()
-  file = File.open("tutorial_file/#{read_title}.txt", "r") 
-  puts file.read
-  file.close()
-rescue
-  puts "file not found"
-end
-
-
-
